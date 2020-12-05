a2a325f831262025a4b7d4c9b25e59db0919ae01
Chandra Shekar <schandra@iotric.com>
2020-12-06 03:45:25 +0530

    IP address change

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   Login_User:"LOGIN_USER",
-  url:"http://54.166.101.176:8081/api/",
-  loginurl:"http://54.166.101.176:8081/adminapi/login",
-  companyurl:"http://54.166.101.176:8081/companyapi/company",
+  url:"http://3.139.89.46:8081/api/",
+  loginurl:"http://3.139.89.46:8081/adminapi/login",
+  companyurl:"http://3.139.89.46:8081/companyapi/company",
 };
